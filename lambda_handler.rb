564156e82570302fc739ee5d42af107ac432d6c5
Samuel Shadwell <smshdwll@gmail.com>
2020-04-19 15:30:56 -0700

    Fix style.

--- lambda_handler.rb
+++ lambda_handler.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 require_relative 'jobs/snapshot_posts_job'
 
-def handle(event:, context:)
+def handle(*)
   snapshot_time = Time.gm(2020, 'apr', 15, 5)
   SnapshotPostsJob.new.run(time: snapshot_time)
 end
