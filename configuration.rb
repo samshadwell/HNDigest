837648995d865b88d12860bd0ade4af6eec06c84
Samuel Shadwell <smshdwll@gmail.com>
2020-04-26 21:05:31 -0700

    Add configuration module.

---
+++ configuration.rb
@@ -0,0 +1,4 @@
+module Configuration
+  TOP_K_VALUES = [10, 20, 50].freeze
+  POINT_THRESHOLD_VALUES = [500, 250, 100].freeze
+end
--- jobs/snapshot_posts_job.rb
+++ jobs/snapshot_posts_job.rb
@@ -2,22 +2,19 @@
 
 require_relative '../lib/post_fetcher'
 require_relative '../lib/storage_adapter'
+require_relative '../configuration'
 
 class SnapshotPostsJob
-  # TODO: Where to put these?
-  TOP_K_VALUES = [10, 20, 50].freeze
-  POINT_THRESHOLD_VALUES = [500, 250, 100].freeze
-
   LOOKBACK = 2 * 24 * 60 * 60 # 2 days in seconds.
   private_constant :LOOKBACK
 
   def run(time:)
     # 2x top K in case all the top k were sent yesterday.
-    posts = PostFetcher.fetch(top_k: 2 * TOP_K_VALUES.max,
-                              points: POINT_THRESHOLD_VALUES.min,
+    posts = PostFetcher.fetch(top_k: 2 * Configuration::TOP_K_VALUES.max,
+                              points: Configuration::POINT_THRESHOLD_VALUES.min,
                               since: time - LOOKBACK)
 
-    storage = StorageAdapter.new
-    storage.snapshot_posts(posts: posts, time: time)
+    # storage = StorageAdapter.new
+    # storage.snapshot_posts(posts: posts, time: time)
   end
 end
