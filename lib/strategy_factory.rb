5d9244eb08eb4ad2c15b7580948583aa568b65ad
Samuel Shadwell <smshdwll@gmail.com>
2020-05-02 19:19:13 -0700

    Add strategy factory.

---
+++ lib/strategy_factory.rb
@@ -0,0 +1,18 @@
+require_relative '../configuration'
+require_relative 'strategies/over_point_threshold'
+require_relative 'strategies/top_n_posts'
+
+class StrategyFactory
+  def self.all_strategies
+    strategies = []
+    Configuration::TOP_N_VALUES.each do |n|
+      strategies << Strategies::TopNPosts.new(n)
+    end
+
+    Configuration::POINT_THRESHOLD_VALUES.each do |threshold|
+      strategies << Strategies::OverPointThreshold.new(threshold)
+    end
+
+    strategies
+  end
+end
