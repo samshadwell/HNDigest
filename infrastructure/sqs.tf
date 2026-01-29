fb82ffaf1c7808fab87eb09690a58bb1b6a3f8de
Sam Shadwell <hi@samshadwell.com>
2026-01-28 20:00:48 -0500

    Add undeliverable notifications to DLQ

--- infrastructure/sqs.tf
+++ infrastructure/sqs.tf
@@ -6,3 +6,27 @@ resource "aws_sqs_queue" "bounce_handler_dlq" {
   name                      = "${lower(var.project_name)}${each.value.name_suffix}-bounce-handler-dlq"
   message_retention_seconds = 1209600 # 14 days
 }
+
+# Allow SNS to send undeliverable messages to the DLQ
+resource "aws_sqs_queue_policy" "bounce_handler_dlq" {
+  for_each = local.environments
+
+  queue_url = aws_sqs_queue.bounce_handler_dlq[each.key].id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = { Service = "sns.amazonaws.com" }
+        Action    = "sqs:SendMessage"
+        Resource  = aws_sqs_queue.bounce_handler_dlq[each.key].arn
+        Condition = {
+          ArnEquals = {
+            "aws:SourceArn" = aws_sns_topic.ses_notifications[each.key].arn
+          }
+        }
+      }
+    ]
+  })
+}
